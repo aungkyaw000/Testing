092d21528c7c1bfc9878ea1e254a9fd03972c05f
aungkyaw000 <152984523+aungkyaw000@users.noreply.github.com>
2025-09-19 11:46:21 +0630

    Create play.dart

---
+++ play.dart
@@ -0,0 +1,298 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+// #docregion platform_imports
+// Import for Android features.
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+// #enddocregion platform_imports
+
+const String kNavigationExamplePage = '''
+<!DOCTYPE html><html>
+<head><title>Navigation Delegate Example</title></head>
+<body>
+<p>
+The navigation delegate is set to block navigation to the youtube website.
+</p>
+<ul>
+<ul><a href="https://www.youtube.com/">https://www.youtube.com/</a></ul>
+<ul><a href="https://www.google.com/">https://www.google.com/</a></ul>
+</ul>
+</body>
+</html>
+''';
+
+const String kLocalExamplePage = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Load file or HTML string example</title>
+</head>
+<body>
+
+<h1>Local demo page</h1>
+<p>
+  This is an example page used to demonstrate how to load a local file or HTML
+  string using the <a href="https://pub.dev/packages/webview_flutter">Flutter
+  webview</a> plugin.
+</p>
+
+</body>
+</html>
+''';
+
+const String kTransparentBackgroundPage = '''
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Transparent background test</title>
+  </head>
+  <style type="text/css">
+    body { background: transparent; margin: 0; padding: 0; }
+    #container { position: relative; margin: 0; padding: 0; width: 100vw; height: 100vh; }
+    #shape { background: red; width: 200px; height: 200px; margin: 0; padding: 0; position: absolute; top: calc(50% - 100px); left: calc(50% - 100px); }
+    p { text-align: center; }
+  </style>
+  <body>
+    <div id="container">
+      <p>Transparent background test</p>
+      <div id="shape"></div>
+    </div>
+  </body>
+  </html>
+''';
+
+const String kLogExamplePage = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Load file or HTML string example</title>
+</head>
+<body onload="console.log('Logging that the page is loading.')">
+
+<h1>Local demo page</h1>
+<p>
+  This page is used to test the forwarding of console logs to Dart.
+</p>
+
+<style>
+    .btn-group button {
+      padding: 24px; 24px;
+      display: block;
+      width: 25%;
+      margin: 5px 0px 0px 0px;
+    }
+</style>
+
+<div class="btn-group">
+    <button onclick="console.error('This is an error message.')">Error</button>
+    <button onclick="console.warn('This is a warning message.')">Warning</button>
+    <button onclick="console.info('This is a info message.')">Info</button>
+    <button onclick="console.debug('This is a debug message.')">Debug</button>
+    <button onclick="console.log('This is a log message.')">Log</button>
+</div>
+
+</body>
+</html>
+''';
+
+class BodyPlayPage extends StatefulWidget {
+  final link;
+  const BodyPlayPage({super.key, this.link});
+
+  @override
+  State<BodyPlayPage> createState() => _BodyPlayPageState();
+}
+
+class _BodyPlayPageState extends State<BodyPlayPage> {
+  late final WebViewController _controller;
+  bool isLoading = true;
+int process = 0;
+  @override
+  void initState() {
+    super.initState();
+
+    // #docregion platform_features
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('WebView is loading (progress : $progress%)');
+            setState(() {
+              process = progress;
+              progress == 100 ? isLoading = false : true;
+            });
+          },
+          onPageStarted: (String url) {
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('''
+Page resource error:
+  code: ${error.errorCode}
+  description: ${error.description}
+  errorType: ${error.errorType}
+  isForMainFrame: ${error.isForMainFrame}
+          ''');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              debugPrint('blocking navigation to ${request.url}');
+              return NavigationDecision.prevent;
+            }
+            debugPrint('allowing navigation to ${request.url}');
+            return NavigationDecision.navigate;
+          },
+          onUrlChange: (UrlChange change) {
+            debugPrint('url change to ${change.url}');
+          },
+          onHttpAuthRequest: (HttpAuthRequest request) {},
+        ),
+      )
+      ..addJavaScriptChannel(
+        'Toaster',
+        onMessageReceived: (JavaScriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse(widget.link.toString()));
+
+    // #docregion platform_features
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    // #enddocregion platform_features
+
+    _controller = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        Future<bool> backFun() async {
+          if (await _controller.canGoBack()) {
+            await _controller.goBack();
+            return false;
+          } else {
+            if (context.mounted) {
+              // ScaffoldMessenger.of(context).showSnackBar(
+              //   const SnackBar(content: Text('No back history item')),
+              // );
+            }
+            return true;
+          }
+        }
+
+        return backFun();
+      },
+      child: SafeArea(
+        child: Scaffold(
+          appBar: widget.link == "https://www.flashfootball.com/"
+              ? null
+              : AppBar(
+                  title: const Text('မောင်းဘော်ဒီကစားရန်'),
+                  // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
+                  // actions: <Widget>[
+                  //   NavigationControls(webViewController: _controller),
+                  //   // SampleMenu(webViewController: _controller),
+                  // ],
+                ),
+          body: Stack(
+            children: [
+              isLoading
+                  ?  Center(child: CircularProgressIndicator(value: process.toDouble()/100,))
+                  : Container(),
+              RefreshIndicator(
+                onRefresh: () async {
+                  _controller.reload();
+                },
+                child: WebViewWidget(
+                  controller: _controller,
+                ),
+              ),
+
+            ],
+          ),
+
+          // WebViewWidget(controller: _controller),
+        ),
+      ),
+    );
+  }
+}
+
+// class NavigationControls extends StatelessWidget {
+//   const NavigationControls({super.key, required this.webViewController});
+//
+//   final WebViewController webViewController;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: <Widget>[
+//         IconButton(
+//           icon: const Icon(Icons.arrow_back_ios),
+//           onPressed: () async {
+//             if (await webViewController.canGoBack()) {
+//               await webViewController.goBack();
+//             } else {
+//               if (context.mounted) {
+//                 ScaffoldMessenger.of(context).showSnackBar(
+//                   const SnackBar(content: Text('No back history item')),
+//                 );
+//               }
+//             }
+//           },
+//         ),
+//         IconButton(
+//           icon: const Icon(Icons.arrow_forward_ios),
+//           onPressed: () async {
+//             if (await webViewController.canGoForward()) {
+//               await webViewController.goForward();
+//             } else {
+//               if (context.mounted) {
+//                 ScaffoldMessenger.of(context).showSnackBar(
+//                   const SnackBar(content: Text('No forward history item')),
+//                 );
+//               }
+//             }
+//           },
+//         ),
+//         IconButton(
+//           icon: const Icon(Icons.replay),
+//           onPressed: () => webViewController.reload(),
+//         ),
+//       ],
+//     );
+//   }
+// }
